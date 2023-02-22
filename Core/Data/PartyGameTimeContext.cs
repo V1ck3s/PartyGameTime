b75b2a38e7f5d32e618d4b691bccb8ea4a267ed1
VickeS <vic1997@users.noreply.github.com>
2023-02-22 22:50:48 +0100

    Database : Adding DbSet to the context.

--- Core/Data/PartyGameTimeContext.cs
+++ Core/Data/PartyGameTimeContext.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using PartyGameTime.Core.Model;
 
 namespace PartyGameTime.Core.Data;
 
@@ -8,4 +10,19 @@ public class PgtDbContext : DbContext
     {
         
     }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.Entity<IdentityUserRole<string>>(eb=>
+            eb.HasNoKey()
+            );
+    }
+    
+    public DbSet<Account> Accounts { get; set; }
+    public DbSet<AccountRole> AccountRoles { get; set; }
+    public DbSet<IdentityUserClaim<string>> IdentityUserClaims { get; set; }
+    public DbSet<IdentityUserRole<string>> IdentityUserRoles { get; set; }
+    
 }
